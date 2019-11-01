79f90c89f22a0a6094a4c10fcd1eeb0170ea0b95
Suyatna <zuyatna@gmail.com>
2019-11-02 03:53:35 +0700

    adding panel menu

---
+++ Assets/Scripts/MenuOpener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MenuOpener : MonoBehaviour
+{
+    public GameObject menu;
+
+    public void OpenPanelMenu()
+    {
+        if(menu != null)
+        {
+            Animator animator = menu.GetComponent<Animator>();
+            if(animator != null)
+            {
+                bool isOPen = animator.GetBool("Open");
+                animator.SetBool("Open", !isOPen);
+            }
+        }
+    }
+}
